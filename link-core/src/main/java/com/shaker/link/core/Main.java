28ed7d8dd852c2f50caf55392b06d51d076c2f0f
yinghuihong <yhhong1943@gmail.com>
2016-07-13 21:51:42 +0800

    Init project

---
+++ link-core/src/main/java/com/shaker/link/core/Main.java
@@ -0,0 +1,11 @@
+package com.shaker.link.core;
+
+/**
+ * Created by yinghuihong on 16/7/13.
+ */
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Hello shaker link.");
+    }
+}
